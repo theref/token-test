c642808600e4c508de802f599f8b56e0174cc7a9
James Campbell <james.campbell@tanti.org.uk>
2022-02-16 18:58:06 +0000

    Clean up tests by moving deployment to separate function

--- tests/test_token_swap.py
+++ tests/test_token_swap.py
@@ -14,37 +14,26 @@ def coffee(CoffeeToken, accounts):
     return CoffeeToken.deploy({"from": accounts[0]})
 
 
-@pytest.fixture
-def swap(beer, coffee, accounts):
-    return TokenSwap.deploy(beer.address, coffee.address, 1, 1, {"from": accounts[0]})
-
-
-@pytest.fixture
-def decimal_swap(beer, coffee, accounts):
-    return TokenSwap.deploy(beer.address, coffee.address, 3, 2, {"from": accounts[0]})
-
-
-@pytest.fixture
-def unbalanced_swap(beer, coffee, accounts):
-    # 2 beer tokens are worth 1 coffee token
-    return TokenSwap.deploy(beer.address, coffee.address, 2, 1, {"from": accounts[0]})
-
-
-def test_deploy_token_swap(beer, coffee, swap):
-    tx = beer.getBeer({"from": swap.address})
-    tx.wait(1)
-    assert beer.balanceOf(swap.address) == 100
-    tx = coffee.getCoffee({"from": swap.address})
-    tx.wait(1)
-    assert coffee.balanceOf(swap.address) == 100
-
-
-def test_swap(beer, coffee, swap):
+def setup_swap(beer, coffee, beer_ratio, coffee_ratio):
+    swap = TokenSwap.deploy(
+        beer.address, coffee.address, beer_ratio, coffee_ratio, {"from": accounts[0]}
+    )
     user = accounts[1]
     tx = beer.getBeer({"from": swap.address})
     tx = beer.getBeer({"from": user})
     tx = coffee.getCoffee({"from": swap.address})
     tx.wait(1)
+    return swap, user
+
+
+def test_deploy_token_swap(beer, coffee):
+    swap, _ = setup_swap(beer, coffee, 1, 1)
+    assert beer.balanceOf(swap.address) == 100
+    assert coffee.balanceOf(swap.address) == 100
+
+
+def test_swap(beer, coffee):
+    swap, user = setup_swap(beer, coffee, 1, 1)
     tx = beer.approve(swap.address, 10, {"from": user})
     tx.wait(1)
     tx = swap.swapAforB(10, {"from": user})
@@ -53,40 +42,28 @@ def test_swap(beer, coffee, swap):
     assert coffee.balanceOf(user) == 10
 
 
-def test_unbalanced_swap(beer, coffee, unbalanced_swap):
-    user = accounts[1]
-    tx = beer.getBeer({"from": unbalanced_swap.address})
-    tx = beer.getBeer({"from": user})
-    tx = coffee.getCoffee({"from": unbalanced_swap.address})
+def test_unbalanced_swap(beer, coffee):
+    swap, user = setup_swap(beer, coffee, 2, 1)
+    tx = beer.approve(swap.address, 10, {"from": user})
     tx.wait(1)
-    tx = beer.approve(unbalanced_swap.address, 10, {"from": user})
-    tx.wait(1)
-    tx = unbalanced_swap.swapAforB(10, {"from": user})
+    tx = swap.swapAforB(10, {"from": user})
     tx.wait(1)
     assert beer.balanceOf(user) == 90
     assert coffee.balanceOf(user) == 5
 
 
-def test_decimal_swap(beer, coffee, decimal_swap):
-    user = accounts[1]
-    tx = beer.getBeer({"from": decimal_swap.address})
-    tx = beer.getBeer({"from": user})
-    tx = coffee.getCoffee({"from": decimal_swap.address})
+def test_decimal_swap(beer, coffee):
+    swap, user = setup_swap(beer, coffee, 3, 2)
+    tx = beer.approve(swap.address, 10, {"from": user})
     tx.wait(1)
-    tx = beer.approve(decimal_swap.address, 10, {"from": user})
-    tx.wait(1)
-    tx = decimal_swap.swapAforB(10, {"from": user})
+    tx = swap.swapAforB(10, {"from": user})
     tx.wait(1)
     assert beer.balanceOf(user) == 91
     assert coffee.balanceOf(user) == 6
 
 
-def test_cannot_swap_without_allowance(beer, coffee, swap):
-    user = accounts[1]
-    tx = beer.getBeer({"from": swap.address})
-    tx = beer.getBeer({"from": user})
-    tx = coffee.getCoffee({"from": swap.address})
-    tx.wait(1)
+def test_cannot_swap_without_allowance(beer, coffee):
+    swap, user = setup_swap(beer, coffee, 1, 1)
     tx = beer.approve(swap.address, 0, {"from": user})
     tx.wait(1)
     with brownie.reverts():
