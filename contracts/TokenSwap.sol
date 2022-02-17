716bec18028fbb8497f9c18fe5c0af6fedd28d51
James Campbell <james.campbell@tanti.org.uk>
2022-02-17 18:29:31 +0000

    Use GCD to simplify the incoming token ratio

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -4,10 +4,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BasicToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    uint8 internal _decimals;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 decimals
+    ) ERC20(_name, _symbol) {
+        _decimals = decimals;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
 
     function faucet() public {
-        _mint(msg.sender, 100);
+        _mint(msg.sender, 100000);
     }
 }
 
@@ -26,11 +38,27 @@ contract TokenSwap {
     ) {
         TokenA = IERC20(_tokenA);
         TokenB = IERC20(_tokenB);
-        TokenARatio = _tokenARatio;
-        TokenBRatio = _tokenBRatio;
+        uint256 _GCD = _GCD(_tokenARatio, _tokenBRatio);
+        TokenARatio = _tokenARatio / _GCD;
+        TokenBRatio = _tokenBRatio / _GCD;
+    }
+
+    function _GCD(uint256 a, uint256 b) internal returns (uint256) {
+        if (a == b) {
+            return a;
+        }
+        if (a > b) {
+            return _GCD(a - b, b);
+        } else {
+            return _GCD(a, b - a);
+        }
     }
 
     function _AtoB(uint256 amount) internal view returns (uint256, uint256) {
+        require(
+            amount > TokenARatio,
+            "Amount must be larger, the ratios don't work"
+        );
         uint256 useable = amount - (amount % TokenARatio);
         return ((useable * TokenBRatio) / TokenARatio, useable);
     }
--- tests/test_token_swap.py
+++ tests/test_token_swap.py
@@ -1,3 +1,4 @@
+from unicodedata import decimal
 import brownie
 from brownie import accounts, BasicToken, TokenSwap
 from web3 import Web3
@@ -6,12 +7,22 @@ import pytest
 
 @pytest.fixture
 def beer(accounts):
-    return BasicToken.deploy("Beer Token", "BEER", {"from": accounts[0]})
+    return BasicToken.deploy("Beer Token", "BEER", 18, {"from": accounts[0]})
 
 
 @pytest.fixture
 def coffee(accounts):
-    return BasicToken.deploy("Coffee Token", "COFF", {"from": accounts[0]})
+    return BasicToken.deploy("Coffee Token", "COFF", 18, {"from": accounts[0]})
+
+
+@pytest.fixture
+def house(accounts):
+    return BasicToken.deploy("House Token", "HOUS", 2, {"from": accounts[0]})
+
+
+@pytest.fixture
+def car(accounts):
+    return BasicToken.deploy("Car Token", "CAR", 5, {"from": accounts[0]})
 
 
 def setup_swap(beer, coffee, beer_ratio, coffee_ratio):
@@ -28,8 +39,8 @@ def setup_swap(beer, coffee, beer_ratio, coffee_ratio):
 
 def test_deploy_token_swap(beer, coffee):
     swap, _ = setup_swap(beer, coffee, 1, 1)
-    assert beer.balanceOf(swap.address) == 100
-    assert coffee.balanceOf(swap.address) == 100
+    assert beer.balanceOf(swap.address) == 100000
+    assert coffee.balanceOf(swap.address) == 100000
 
 
 def test_swap(beer, coffee):
@@ -38,7 +49,7 @@ def test_swap(beer, coffee):
     tx.wait(1)
     tx = swap.swapAforB(10, {"from": user})
     tx.wait(1)
-    assert beer.balanceOf(user) == 90
+    assert beer.balanceOf(user) == 99990
     assert coffee.balanceOf(user) == 10
 
 
@@ -48,7 +59,7 @@ def test_unbalanced_swap(beer, coffee):
     tx.wait(1)
     tx = swap.swapAforB(10, {"from": user})
     tx.wait(1)
-    assert beer.balanceOf(user) == 90
+    assert beer.balanceOf(user) == 99990
     assert coffee.balanceOf(user) == 5
 
 
@@ -58,23 +69,32 @@ def test_non_integer_swap(beer, coffee):
     tx.wait(1)
     tx = swap.swapAforB(10, {"from": user})
     tx.wait(1)
-    assert beer.balanceOf(user) == 91
+    assert beer.balanceOf(user) == 99991
     assert coffee.balanceOf(user) == 6
 
 
-def test_decimal_swap(beer, coffee):
+def test_decimal_swap(house, car):
     """Handling of decimals should be done off-chain, not on-chain.
     With the current setup that is easy, the ratio values just need to be
     adjusted to match the decimal values of the tokens.
+    Assume a House token with 2 decimal places, and a car token with 5 decimal places.
+    Also assume that the ratio is 3:20 (3 House tokens for every 20 car tokens).
+
+    The contracts internal representation needs to be 300000:2000 (3 * 10 ** 5 : 20 * 10 ** 2).
 
     """
-    swap, user = setup_swap(beer, coffee, 3, 2)
-    tx = beer.approve(swap.address, 10, {"from": user})
+    swap, user = setup_swap(
+        house, car, 3 * 10 ** car.decimals(), 20 * 10 ** house.decimals()
+    )
+    tx = house.approve(swap.address, 3000, {"from": user})
     tx.wait(1)
-    tx = swap.swapAforB(10, {"from": user})
+    tx = swap.swapAforB(3000, {"from": user})
     tx.wait(1)
-    assert beer.balanceOf(user) == 91
-    assert coffee.balanceOf(user) == 6
+    assert house.balanceOf(user) == 97000
+    assert car.balanceOf(user) == 20
+    with brownie.reverts():
+        tx = swap.swapAforB(2, {"from": user})
+        tx.wait(1)
 
 
 def test_cannot_swap_without_allowance(beer, coffee):
